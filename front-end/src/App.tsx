a834a751d85ebec25f1acb8b94182c44aa7e322d
dudwns0921 <dudwns0921@gmail.com>
2025-07-09 22:30:55 +0900

    refactor: 인사말 상수화

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,33 +5,19 @@ import useSpeechRecognition from './hooks/useSpeechRecognition';
 import ActionButton from './components/ActionButton/ActionButton';
 import usePost from './hooks/usePost';
 import Flash from './components/Flash/Flash';
-
-const defaultMessages: string[] = [
-  '안녕하세요?',
-  '제게 인사를 건네주세요'
-];
-
-// 심즈 스타일의 독특한 로딩 문구 배열
-const loadingMessages = [
-  '자, 그럼 이제 당신에 대해 더 알아볼게요...',
-  '당신의 매력을 분석하는 중이에요...',
-  '인공지능이 당신의 미소를 해석하고 있어요!',
-  '감탄할 준비를 하는 중입니다...',
-  '유니크함을 데이터베이스에 저장 중...',
-  '당신의 인사를 복습하고 있어요!',
-  '곧 멋진 결과를 보여드릴게요...',
-  '웃음 바이러스를 전파하는 중...',
-  '당신의 긍정 에너지를 측정 중입니다!',
-  'AI가 감동받고 있어요...',
-  '로딩 중... 잠시만 기다려주세요!'
-];
+import {
+  DEFAULT_MESSAGES,
+  GREETING_MESSAGES,
+  LOADING_MESSAGES,
+  COUNTDOWN_MESSAGES
+} from './constants/messages';
 
 const App: React.FC = () => {
   const {
     showMessage,
     currentMessage,
     setAnimatedMessages
-  } = useAnimatedMessages(defaultMessages);
+  } = useAnimatedMessages(DEFAULT_MESSAGES);
 
   // isGreeting 상태 추가
   const [isGreeting, setIsGreeting] = useState(false);
@@ -50,10 +36,10 @@ const App: React.FC = () => {
   const handleSpeechResult = async (text: string) => {
     const response = await post({ text });
     if(response.is_greeting) {
-      setAnimatedMessages(['저에게 인사를 해주셨군요!', '이제 당신의 모습을 사진으로 담을게요!'])
+      setAnimatedMessages(GREETING_MESSAGES);
       setIsGreeting(true); // 인사 감지 시 true로 변경
     } else {
-      setAnimatedMessages(response.reason.split('.'))
+      setAnimatedMessages(response.reason.split('.'));
       setIsGreeting(false); // 인사가 아니면 false로 변경
     }
   };
@@ -69,13 +55,12 @@ const App: React.FC = () => {
       clearTimeout(flashTimerRef.current);
       flashTimerRef.current = null;
     }
-    const countdownMessages = ['하나', '둘', '셋!'];
-    setAnimatedMessages(countdownMessages);
+    setAnimatedMessages(COUNTDOWN_MESSAGES);
     cameraTimerRef.current = setTimeout(() => {
       setIsFlash(true);
       flashTimerRef.current = setTimeout(() => {
         setIsFlash(false);
-        setAnimatedMessages(loadingMessages);
+        setAnimatedMessages(LOADING_MESSAGES);
       }, 200);
     }, 5400);
   };
