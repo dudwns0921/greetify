66cba1688a497c6c89052166110bea72cfd40491
dudwns0921 <dudwns0921@gmail.com>
2025-07-09 21:49:50 +0900

    feat: 백엔드 인사말 판단 api 추가

--- back-end/app/api/api.py
+++ back-end/app/api/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
-from app.api.endpoints import greet_from_image
+from app.api.endpoints import greet_from_image, greet_from_text
 
 api_router = APIRouter()
 
 api_router.include_router(
     greet_from_image.router
 )
+
+api_router.include_router(greet_from_text.router)
