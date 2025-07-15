10ce121b6f3f2d52a2b0ad1335cba7853a518a42
dudwns0921 <dudwns0921@gmail.com>
2025-07-15 22:05:41 +0900

    chore: netlify proxy 함수 수정 및 node-fetch 라이브러리 추가

--- front-end/netlify/functions/api-proxy.js
+++ front-end/netlify/functions/api-proxy.js
@@ -1,8 +1,15 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 exports.handler = async (event) => {
-  const path = event.path.replace('/.netlify/functions/api-proxy', '');
+  // '/.netlify/functions/api-proxy' 또는 '/api'를 제거
+  let path = event.path;
+  if (path.startsWith('/.netlify/functions/api-proxy')) {
+    path = path.replace('/.netlify/functions/api-proxy', '');
+  } else if (path.startsWith('/api')) {
+    path = path.replace('/api', '');
+  }
   const url = `http://158.179.167.189:8000/api/v1${path}`;
+  console.log('프록시 요청 URL:', url);
 
   const response = await fetch(url, {
     method: event.httpMethod,
