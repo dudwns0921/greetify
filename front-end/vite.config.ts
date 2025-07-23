23ab483bf146b70839ea23c1b2658513ea843874
elio.Jung <elio.Jung@obigo.com>
2025-07-24 08:31:17 +0900

    chore: deploy.yml 백엔드 배포 관련 로직 제거, netlify proxy 제거, vite config host 옵션 추가

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
   base: '/greetify/',
   plugins: [react()],
   resolve: {
