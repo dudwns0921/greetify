f3324e5483effcc00893c12b54696ee6253eade0
elio.Jung <elio.Jung@obigo.com>
2025-07-23 12:09:48 +0900

    chore: 도커 설정 파일 및 github action 파일 추가

--- front-end/src/util/server.ts
+++ front-end/src/util/server.ts
@@ -1,6 +1,3 @@
 export const getServerUrl = () => {
-    if (import.meta.env.DEV) {
-        return 'http://localhost:8000/api/v1';
-    }
-    return '/api';
+    return 'http://localhost:8000/api/v1';
 }
